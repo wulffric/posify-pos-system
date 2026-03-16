71de47190b28caea88d519bbf7ea91fb3a3c2861
Xyrylle026 <maputolx@gmail.com>
2026-03-15 22:58:18 -0400

    Update calculateTotal.js

--- backend/utils/calculateTotal.js
+++ backend/utils/calculateTotal.js
@@ -1,1 +1,11 @@
+function calculateTotal(items) {
+  let total = 0;
 
+  items.forEach(item => {
+    total += item.price * item.quantity;
+  });
+
+  return total;
+}
+
+module.exports = calculateTotal;

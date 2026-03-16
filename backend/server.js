81f6f6a003fd49ff030077bbdd775f4060ce880a
simmie057 <simmie057@aol.com>
2026-03-15 18:35:41 -0700

    create back end server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "POSify backend running" });
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

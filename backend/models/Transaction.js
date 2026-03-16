6caca9f90713add0c28140fe8b9a0640ded77fd6
Xyrylle026 <maputolx@gmail.com>
2026-03-15 23:00:51 -0400

    Create Transaction.js

---
+++ backend/models/Transaction.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  total: {
+    type: Number,
+    required: true
+  },
+  paymentType: {
+    type: String,
+    enum: ["cash", "card"],
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now

bc3959849a8cecfc6dd90dd20c0cc37dc027e191
Pooja Vyas <pvyas13@my.centennialcollege.ca>
2026-03-31 17:58:45 -0400

    Removed merge conflict code

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,41 +1,5 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-const transactionSchema = new mongoose.Schema({
-  items: [
-    {
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true
-      },
-      name: {
-        type: String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      quantity: {
-        type: Number,
-        required: true
-      }
-    }
-  ],
-  total: {
-    type: Number,
-    required: true
-  },
-  paymentType: {
-    type: String,
-    enum: ["cash", "card"],
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-=======
 const TransactionSchema = new mongoose.Schema({
     items: [
         {
@@ -78,4 +42,3 @@ const TransactionSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
->>>>>>> d15e768 (Iteration 2 backend complete: login, role management, refund, user control)
